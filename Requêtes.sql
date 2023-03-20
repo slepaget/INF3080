7335383d1c08f91aec3b5decb4e09c829131b125
Francis <ff291081@ens.uqam.ca>
2023-03-19 23:03:30 -0400

    Ajout des solutions pour numeros paires

--- Requêtes.sql
+++ Requêtes.sql
@@ -5,6 +5,10 @@ WHERE no_client IN (
     SELECT no_client FROM Commande);
 
 --Requête 2.2
+SELECT no_commande, date_commande, nom, prenom, telephone
+FROM Commande, Client
+WHERE Commande.no_client = Client.no_client AND date_commande BETWEEN '23-02-02' 
+AND '23-03-10'
 
 --Requête 2.3
 SELECT 
@@ -22,6 +26,16 @@ AND no_client IN (
 ORDER BY commande.no_commande ASC;
 
 --Requête 2.4
+SELECT Produit.no_produit, Produit.description, Fournisseur.raison_sociale, Fournisseur.telephone
+FROM Produit, Fournisseur, Approvisionnement
+WHERE Approvisionnement.code_fournisseur = Fournisseur.code_fournisseur AND Approvisionnement.no_produit = Produit.no_produit
+
+--A revoir***
+SELECT Produit.description, COUNT(DISTINCT Produit.code_fournisseur_prioritaire) AS nombre_fournisseurs
+FROM Produit, Approvisionnement
+WHERE Produit.no_produit = Approvisionnement.no_produit
+GROUP BY Produit.description
+ORDER BY Produit.description ASC;
 
 --Requête 2.5
 SELECT 
@@ -32,6 +46,15 @@ INNER JOIN Prix_Produit ON produit.no_produit = prix_produit.no_produit
 ORDER BY prix_produit.date_envigueur ASC;
 
 --Requête 2.6
+SELECT Livraison.no_livraison, date_livraison, date_paiement, montant, type_paiement
+FROM Livraison, Paiement
+WHERE Livraison.no_livraison = Paiement.no_livraison AND Livraison.no_livraison = 50021
+
+SELECT Livraison.no_livraison, SUM(Paiement.montant) AS total_paiements, COUNT(Paiement.id_paiement) AS nombre_paiements
+FROM Livraison, Paiement
+WHERE Livraison.no_livraison = Paiement.no_livraison
+GROUP BY Livraison.no_livraison
+ORDER BY Livraison.no_livraison;
 
 --Requête 3.1
 CREATE OR REPLACE TRIGGER R31_IdPaiement
@@ -56,6 +79,31 @@ SELECT * FROM Paiement WHERE nom_banque='Requête3.1 Bank'
 
 
 --Requête 3.2
+
+CREATE VIEW V_commande_item AS
+SELECT nom, prenom, Commande.no_commande, Ligne_commande.quantite_cmd, Prix_produit.prix_unitaire, Commande.statut
+FROM Commande, Prix_produit, Client, Ligne_commande, Produit
+WHERE Commande.no_client = Client.no_client AND Commande.no_commande = Ligne_commande.no_commande AND Ligne_commande.no_produit = Produit.no_produit AND Produit.no_produit = Prix_produit.no_produit
+
+SELECT * 
+FROM V_commande_item;
+
+CREATE OR REPLACE VIEW V_commande_item AS
+SELECT nom, prenom, Commande.no_commande, Ligne_commande.quantite_cmd, Prix_produit.prix_unitaire, Commande.statut, (Ligne_commande.quantite_cmd * prix_produit.prix_unitaire) AS Prix_Total_item
+FROM Commande, Prix_produit, Client, Ligne_commande, Produit
+WHERE Commande.no_client = Client.no_client AND Commande.no_commande = Ligne_commande.no_commande AND Ligne_commande.no_produit = Produit.no_produit AND Produit.no_produit = Prix_produit.no_produit
+
+SELECT * 
+FROM V_commande_item
+WHERE nom = 'Michel' AND prenom = 'Tremblay'
+
+SELECT * 
+FROM V_commande_item
+WHERE no_commande = 300
+
+SELECT SUM(Prix_Total_item) AS Montant_total_commande
+FROM V_commande_item
+WHERE no_commande = 300;
 
 --Requête 3.3
 SELECT DISTINCT statut, COUNT(*) as count FROM Commande
