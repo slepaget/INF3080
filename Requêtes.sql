64b6550a0e6ebe30681e30026aa4b17ff942208f
francis.lewis07@gmail.com <francis.lewis07@gmail.com>
2023-03-24 09:29:38 -0400

    Ajout des commentaires pour identifier les sous numeros

--- Requêtes.sql
+++ Requêtes.sql
@@ -26,10 +26,13 @@ AND no_client IN (
 ORDER BY commande.no_commande ASC;
 
 --Requête 2.4
+
+--a)
 SELECT Produit.no_produit, Produit.description, Fournisseur.raison_sociale, Fournisseur.telephone
 FROM Produit, Fournisseur, Approvisionnement
 WHERE Approvisionnement.code_fournisseur = Fournisseur.code_fournisseur AND Approvisionnement.no_produit = Produit.no_produit
 
+--b)
 --A revoir***
 SELECT Produit.description, COUNT(DISTINCT Produit.code_fournisseur_prioritaire) AS nombre_fournisseurs
 FROM Produit, Approvisionnement
@@ -46,10 +49,13 @@ INNER JOIN Prix_Produit ON produit.no_produit = prix_produit.no_produit
 ORDER BY prix_produit.date_envigueur ASC;
 
 --Requête 2.6
+
+--a)
 SELECT Livraison.no_livraison, date_livraison, date_paiement, montant, type_paiement
 FROM Livraison, Paiement
 WHERE Livraison.no_livraison = Paiement.no_livraison AND Livraison.no_livraison = 50021
 
+--b)
 SELECT Livraison.no_livraison, SUM(Paiement.montant) AS total_paiements, COUNT(Paiement.id_paiement) AS nombre_paiements
 FROM Livraison, Paiement
 WHERE Livraison.no_livraison = Paiement.no_livraison
@@ -80,6 +86,7 @@ SELECT * FROM Paiement WHERE nom_banque='Requête3.1 Bank'
 
 --Requête 3.2
 
+--a)
 CREATE VIEW V_commande_item AS
 SELECT nom, prenom, Commande.no_commande, Ligne_commande.quantite_cmd, Prix_produit.prix_unitaire, Commande.statut
 FROM Commande, Prix_produit, Client, Ligne_commande, Produit
@@ -88,19 +95,25 @@ WHERE Commande.no_client = Client.no_client AND Commande.no_commande = Ligne_com
 SELECT * 
 FROM V_commande_item;
 
+--Modification de la vue dans laquelle on ajoute une colonne pour avoir le prix total de l'item
 CREATE OR REPLACE VIEW V_commande_item AS
 SELECT nom, prenom, Commande.no_commande, Ligne_commande.quantite_cmd, Prix_produit.prix_unitaire, Commande.statut, (Ligne_commande.quantite_cmd * prix_produit.prix_unitaire) AS Prix_Total_item
 FROM Commande, Prix_produit, Client, Ligne_commande, Produit
 WHERE Commande.no_client = Client.no_client AND Commande.no_commande = Ligne_commande.no_commande AND Ligne_commande.no_produit = Produit.no_produit AND Produit.no_produit = Prix_produit.no_produit
 
+
+--b)
 SELECT * 
 FROM V_commande_item
 WHERE nom = 'Michel' AND prenom = 'Tremblay'
 
+--c)
 SELECT * 
 FROM V_commande_item
 WHERE no_commande = 300
 
+
+--d)
 SELECT SUM(Prix_Total_item) AS Montant_total_commande
 FROM V_commande_item
 WHERE no_commande = 300;
@@ -131,6 +144,7 @@ AND date_paiement BETWEEN '2022/11/01' AND '2022/11/30'
 
 --Requête 3.6
 
+--a)
 SELECT MAX(quantite_cmd) AS max_items
 FROM Ligne_commande, (
     SELECT Commande.no_commande, COUNT(Ligne_commande.no_produit) AS nbr_items
@@ -138,6 +152,7 @@ FROM Ligne_commande, (
     GROUP BY Commande.no_commande
 );
 
+--b)
 SELECT Ligne_commande.no_commande
 FROM Ligne_commande
 WHERE Ligne_commande.quantite_cmd = (
@@ -149,6 +164,7 @@ WHERE Ligne_commande.quantite_cmd = (
     )
 );
 
+--c)
 SELECT Commande.no_commande, date_commande, Commande.statut, Client.no_client, nom, prenom
 FROM Commande, Client
 WHERE Commande.no_client = Client.no_client AND Commande.no_commande = (
