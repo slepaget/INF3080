06c6f63763ec56544c7ee2e8e536f78186b2552a
slepaget <simonlepagetrudeau@hotmail.com>
2023-04-10 18:08:19 -0400

    renamed

--- TP2_requêteSerieII.sql
+++ TP2_requêteSerieII.sql
@@ -15,11 +15,7 @@ BEGIN
     :NEW.id_paiement := id;
 END;
 
-INSERT INTO Paiement (date_paiement,montant,type_paiement,no_cheque,nom_banque,no_carte_credit,type_carte_credit,no_livraison) 
-    VALUES(SYSDATE,1.25,'CASH', NULL, 'Requête3.1 Bank', NULL, NULL,751);
-
 SELECT * FROM Paiement WHERE nom_banque='Requête3.1 Bank'
-
 
 --Requête 3.2
 
@@ -82,52 +78,37 @@ AND date_paiement BETWEEN '2022/11/01' AND '2022/11/30'
 --Requête 3.6
 
 --a)
-SELECT MAX(max_items) as max_items
-FROM (
-    SELECT no_commande, SUM(quantite_cmd) AS max_items
-    FROM (
-        SELECT no_commande, quantite_cmd
-        FROM Ligne_commande)
-    GROUP BY no_commande
+SELECT MAX(quantite_cmd) AS max_items
+FROM Ligne_commande, (
+    SELECT Commande.no_commande, COUNT(Ligne_commande.no_produit) AS nbr_items
+    FROM Commande, Ligne_commande
+    GROUP BY Commande.no_commande
 );
 
 --b)
-SELECT no_commande
-FROM(
-    SELECT no_commande,SUM(quantite_cmd) as qte_max
-    FROM (
-        SELECT no_commande, quantite_cmd
-            FROM Ligne_commande)
-    GROUP BY no_commande
-    ) 
-WHERE qte_max = (SELECT MAX(qte_max) FROM (
-    SELECT no_commande,SUM(quantite_cmd) as qte_max
-    FROM (
-        SELECT no_commande, quantite_cmd
-            FROM Ligne_commande)
-    GROUP BY no_commande
-    ) 
+SELECT Ligne_commande.no_commande
+FROM Ligne_commande
+WHERE Ligne_commande.quantite_cmd = (
+    SELECT MAX(quantite_cmd) AS max_items
+    FROM Ligne_commande, (
+    SELECT Commande.no_commande, COUNT(Ligne_commande.no_produit) AS nbr_items
+    FROM Commande, Ligne_commande
+    GROUP BY Commande.no_commande
+    )
 );
 
 --c)
 SELECT Commande.no_commande, date_commande, Commande.statut, Client.no_client, nom, prenom
 FROM Commande, Client
 WHERE Commande.no_client = Client.no_client AND Commande.no_commande = (
-    SELECT no_commande
-    FROM(
-        SELECT no_commande,SUM(quantite_cmd) as qte_max
-        FROM (
-            SELECT no_commande, quantite_cmd
-            FROM Ligne_commande)
-        GROUP BY no_commande
-    ) 
-    WHERE qte_max = (SELECT MAX(qte_max) 
-    FROM (
-        SELECT no_commande,SUM(quantite_cmd) as qte_max
-        FROM (
-            SELECT no_commande, quantite_cmd
-            FROM Ligne_commande)
-        GROUP BY no_commande
-    ) 
-    )
+    SELECT Ligne_commande.no_commande
+    FROM Ligne_commande
+    WHERE Ligne_commande.quantite_cmd = (
+        SELECT MAX(quantite_cmd) AS max_items
+        FROM Ligne_commande, (
+            SELECT Commande.no_commande, COUNT(Ligne_commande.no_produit) AS nbr_items
+            FROM Commande, Ligne_commande
+            GROUP BY Commande.no_commande
+            )
+        )
 );
