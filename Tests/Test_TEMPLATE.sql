9f00913f04ce8875ce141bf00edfb00829e3bd11
slepaget <simonlepagetrudeau@hotmail.com>
2023-04-13 21:09:38 -0400

    template!

---
+++ Tests/Test_TEMPLATE.sql
@@ -0,0 +1,50 @@
+create or replace PROCEDURE Test_C1
+
+IS
+    R NUMBER;
+    len NUMBER;
+    RArray ARRAY := ARRAY();
+    temp VARCHAR(16);
+
+BEGIN
+len:=0;
+
+--TEST THAT PASSES IF THE COMMAND ***FAILS***
+    BEGIN
+        len := len+1;
+        R :=1;
+
+        --ENTER CODE HERE
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            R := 0;
+    END;
+    RArray.EXTEND(1);RArray(len):=R;ROLLBACK;
+
+
+--TEST THAT PASSES IF THE COMMAND IS EXECUTED CORRECTLY
+    BEGIN
+        len := len+1;
+        R :=0;
+
+        --ENTER CODE HERE
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            R := 1;
+    END;
+    RArray.EXTEND(1);RArray(len):=R;ROLLBACK;
+
+
+--LOOP OVER THE RESULTS AND PRINTS
+    FOR i IN 1..RArray.COUNT LOOP
+        IF RArray(i)= 0 THEN 
+            temp:='PASSED';
+        ELSE 
+            temp:='*****FAILED*****';
+        END IF;
+        DBMS_OUTPUT.PUT_LINE('Test #' || i || '   ' || temp);
+    END LOOP;
+
+END;
