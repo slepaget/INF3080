275e04f8568af8e829fa79ba56f4eb11977bd73e
slepaget <simonlepagetrudeau@hotmail.com>
2023-04-13 20:55:27 -0400

    list of tests

---
+++ TP3_3_Tests.sql
@@ -0,0 +1,28 @@
+--C1 Check status Commande
+
+--C2 date inscription date du jour Produit
+
+--C3 Check qte stock Produit
+
+--C4 Check qte Seuil >=0 Produit
+
+--C5 Check qte_commander >0 Ligne commande
+
+--C6 qte_livrée >0 Ligne livraison
+
+--C7 Check qte approvis Approvisionnement
+
+--C8 Check status Approvisonnement
+
+--C9 Check type de paiement table paiement
+
+--C10 Check type de carte de credit Paiement
+
+--Trigger TP2 r3.1 paiement
+
+--TP3 2.1 
+
+--TP3 2.2 
+
+--TP3 2.3 
+
